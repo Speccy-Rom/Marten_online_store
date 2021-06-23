63b85957f30f8154c2013ca6a2dd4c417627b5b3
Speccy-Rom <speccy.rom@gmail.com>
2021-06-23 23:43:51 +0300

    added page blog and detail post in blog

--- blog_store/urls.py
+++ blog_store/urls.py
@@ -4,6 +4,7 @@ from blog_store import views
 urlpatterns = [
     path('', views.index, name='all_blogs'),
     path('blog/', views.all_blogs, name='all_blogs'),
+    path('category/<int:category_id>/', views.get_category, name='get_category'),
 
 ]
 
